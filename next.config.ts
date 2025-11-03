b6b7a72a5c06fcea819803822e4e8a07c4a8be8a
odidukh <oles.didukh@gmail.com>
2025-11-03 21:05:47 +0200

    chore: update dependencies for Next.js and React
    - upgraded Next.js from 15.4.2-canary.14 to 16.0.1
    - upgraded React and ReactDOM from 19.1.0 to 19.2.0
    - updated @types/react and @types/react-dom to 19.2.2

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   experimental: {
-    reactCompiler: true,
     optimizePackageImports: ['lucide-react', 'framer-motion'],
     webpackBuildWorker: true,
   },
